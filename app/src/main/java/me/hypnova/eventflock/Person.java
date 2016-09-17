8e51d195ff5264de8b0c167dd3ab745d4f5fc057
atarw <atarw11@outlook.com>
2016-09-17 14:29:43 -0400

    added event and person classes

---
+++ app/src/main/java/me/hypnova/eventflock/Person.java
@@ -0,0 +1,13 @@
+package me.hypnova.eventflock;
+
+import java.util.List;
+
+/**
+ * Created by Atharva on 2016-09-17.
+ */
+public class Person {
+    private String name;
+    private String about;
+    private List<Person> liked, disliked;
+
+}
